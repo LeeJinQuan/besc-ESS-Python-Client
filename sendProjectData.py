31dca6702a492f2e0db582d9e8e529ad2c33bdec
vickneshram25 <51814999+vickneshram25@users.noreply.github.com>
2019-06-18 15:54:40 +0800

    Update sendProjectData.py

--- sendProjectData.py
+++ sendProjectData.py
@@ -4,24 +4,48 @@ from device import Device
 import keypair as keyPair
 import datetime
 import time
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+import requests
+import json
+import urllib
+import pprint
+
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 17
+instance = dht11.DHT11(pin=17)
+
 
 keypair = keyPair.KeyPair("STUD001", "gS5pBilMCBS91z0go3LO6iqLUfZCU0DH")
 
 def sendProjectData():
 
     while (True):
-        projectData = ProjectData(
-            "Testing",
-            datetime.datetime.now(),
-            [
-                Device("AABC1", 40.0),
-                Device("AABC2", 70.0)
-            ],
-            110.0,
-            80.0,
-            "Selangor"
-            )
+        result = instance.read()   
+        if result.is_valid():
 
+            projectData = ProjectData(
+                "Testing",
+                datetime.datetime.now(),
+                [
+                    Device("AABC1", result.temperature),
+                    Device("AABC2", result.humidity)
+                ],
+                110.6,
+                80.4,
+                "Selangor"
+                )
+        
+        else:
+            pass
+        
         try:
             print(API.sendProjectData(keypair, projectData))
             time.sleep(10)
@@ -31,3 +55,4 @@ def sendProjectData():
 
 if __name__ == "__main__":
     sendProjectData()
+
