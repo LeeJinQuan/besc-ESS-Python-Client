79a6d5b203d58eda78656cd37474e6ef8cc96001
Lee Jin Quan <51855624+LeeJinQuan@users.noreply.github.com>
2019-06-18 14:48:16 +0800

    Update validate.py

--- validate.py
+++ validate.py
@@ -4,13 +4,15 @@ def number(variable, variable_name):
     if(isinstance(variable, int) != True):
         raise Exception("Invalid data. " + variable_name + " must be a number")
 
+def checkfloat(variable, variable_name):
+    if(type(variable) != float):
+        raise Exception("Invalid data. " + variable_name + " must be a float")
+
 def string(variable, variable_name):
     if(isinstance(variable, str) != True):
         raise Exception("Invalid data. " + variable_name + " must be a string")
 
 def datetimeString(variable, variable_name):
-    if(isinstance(variable, str) != True):
-        raise Exception("Invalid data. " + variable_name + " must be a datetime string")
 
     if(isinstance(variable, datetime.datetime) != True):
         raise Exception("Invalid data. " + variable_name + " must be a datetime string")
