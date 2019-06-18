9903f8c28948d3c0be74490cd4e2c61023036928
Lee Jin Quan <51855624+LeeJinQuan@users.noreply.github.com>
2019-06-18 14:45:58 +0800

    Update sendProjectData.py

--- sendProjectData.py
+++ sendProjectData.py
@@ -3,28 +3,31 @@ from project_data import ProjectData
 from device import Device
 import keypair as keyPair
 import datetime
+import time
 
 keypair = keyPair.KeyPair("STUD001", "gS5pBilMCBS91z0go3LO6iqLUfZCU0DH")
 
 def sendProjectData():
 
-    projectData = ProjectData(
-        "Testing",
-        datetime.datetime.now(),
-        [
-            Device("AABC1", 40),
-            Device("AABC2", 70)
-        ],
-        110,
-        80,
-        "101.1212, 112.1133"
-        )
+    while (True):
+        projectData = ProjectData(
+            "Testing",
+            datetime.datetime.now(),
+            [
+                Device("AABC1", 40.0),
+                Device("AABC2", 70.0)
+            ],
+            110.0,
+            80.0,
+            "Selangor"
+            )
 
-    try:
-        API.sendProjectData(keypair, projectData)
+        try:
+            print(API.sendProjectData(keypair, projectData))
+            time.sleep(10)
 
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     sendProjectData()
