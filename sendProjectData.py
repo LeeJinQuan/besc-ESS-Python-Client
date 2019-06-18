fd2c8eac5177e16d7a48ec41d4a97e58d44da535
Lee Jin Quan <51855624+LeeJinQuan@users.noreply.github.com>
2019-06-18 16:15:28 +0800

    Update sendProjectData.py

--- sendProjectData.py
+++ sendProjectData.py
@@ -2,17 +2,12 @@ import API
 from project_data import ProjectData
 from device import Device
 import keypair as keyPair
-import datetime
-import time
 import RPi.GPIO as GPIO
 import dht11
 import time
 import datetime
-import requests
-import json
-import urllib
-import pprint
-
+import decimal
+import random
 
 # initialize GPIO
 GPIO.setwarnings(False)
@@ -22,37 +17,39 @@ GPIO.cleanup()
 # read data using pin 17
 instance = dht11.DHT11(pin=17)
 
-
 keypair = keyPair.KeyPair("STUD001", "gS5pBilMCBS91z0go3LO6iqLUfZCU0DH")
 
+
 def sendProjectData():
-
     while (True):
-        result = instance.read()   
+        result = instance.read()
         if result.is_valid():
-
-            projectData = ProjectData(
-                "Testing",
-                datetime.datetime.now(),
-                [
-                    Device("AABC1", result.temperature),
-                    Device("AABC2", result.humidity)
-                ],
-                110.6,
-                80.4,
-                "Selangor"
+        
+            temp = float(decimal.Decimal(random.randrange(result.temperature - 1.01, result.temperature + 1.01)))
+            humi = float(decimal.Decimal(random.randrange(result.humidity - 1.01, result.humidity + 1.01)))
+            total = temp + humi
+        
+                projectData = ProjectData(
+                    "Testing",
+                    datetime.datetime.now(),
+                    [
+                        Device("AABC1", temp),
+                        Device("AABC2", humi)
+                    ],
+                    float(decimal.Decimal(random.randrange(total - 1.01, total + 1.01))),
+                    (result.temperature + result.humidity) * 0.28434517,
+                    "Selangor"
                 )
-        
-        else:
-            pass
-        
-        try:
-            print(API.sendProjectData(keypair, projectData))
-            time.sleep(10)
 
-        except Exception as e:
-            print(e)
+            else:
+                pass
+
+            try:
+                print(API.sendProjectData(keypair, projectData))
+                time.sleep(10)
+
+            except Exception as e:
+                print(e)
 
 if __name__ == "__main__":
     sendProjectData()
-
