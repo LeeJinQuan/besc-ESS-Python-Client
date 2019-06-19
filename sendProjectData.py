1f8cb9252f787bf31b6d5c24434fcd608db4f34f
Lee Jin Quan <51855624+LeeJinQuan@users.noreply.github.com>
2019-06-19 17:11:56 +0800

    Update sendProjectData.py

--- sendProjectData.py
+++ sendProjectData.py
@@ -15,41 +15,44 @@ GPIO.cleanup()
 
 # read data using pin 17
 instance = dht11.DHT11(pin=17)
-
 keypair = keyPair.KeyPair("", "")
 
-
 def sendProjectData():
-    while (True):
         result = instance.read()
-        if result.is_valid():
-
-            temp = random.uniform(result.temperature + 0.1, result.temperature + 0.9)
-            humi = random.uniform(result.humidity + 0.1, result.humidity + 0.9)
-
-            total = temp + humi
-
-            projectData = ProjectData(
-                "Testing",
-                datetime.datetime.now(),
-                [
-                    Device("AABC1", round(temp, 1)),
-                    Device("AABC2", round(humi, 1))
-                ],
-                round(total, 1),
-                round((total * 0.28434517), 1),
-                "Selangor"
-            )
-
-        else:
+        
+        while result is None:
             pass
+        
+        temp = round((random.uniform(result.temperature + 0.1, result.temperature + 0.9)), 1)
+        humi = round((random.uniform(result.humidity + 0.1, result.humidity + 0.9)), 1)
+        
+        total = str(round((temp + humi), 1))
+        if (".0" in total):
+            total = round(total) + 0.1
+        else:
+            total = round(float(total), 1)
+        
+        projectData = ProjectData(
+            "Testing",
+            datetime.datetime.today(),
+            [
+                Device("AABC1", temp),
+                Device("AABC2", humi)
+            ],
+            total,
+            round((total * 0.28434517), 1),
+            "Selangor"
+        )
+            
 
         try:
             print(API.sendProjectData(keypair, projectData))
-            time.sleep(10)
+            time.sleep(60)
+            sendProjectData()
 
         except Exception as e:
             print(e)
 
 if __name__ == "__main__":
+
     sendProjectData()
