39c9c88a59d04bafe396a452464275ae1d7ffe95
vickneshram25 <51814999+vickneshram25@users.noreply.github.com>
2019-06-20 17:31:25 +0800

    Update API.py

--- API.py
+++ API.py
@@ -1,73 +1,41 @@
 import besc_host as Host
-
 import requests
-
 import json
-
 import hashlib
-
 from collections import OrderedDict
-
-
+import dashboard
 
 def sendProjectData(keyPair, projectData):
-
     client = Host.BESC_ESS_Host(("http://carboapi.besc.online/besc-data"))
-
     client.setProjectId(keyPair.projectId)
-
     client.setEndPoint(Host.endpoints.get("project"))
 
-
-
     jsonData = serialize(projectData).get("data")
-
-    print(jsonData)
-
     jsonChecksum = serialize(projectData).get("checksum")
 
+    headersDict = OrderedDict([
+            ("apikey", keyPair.apiKey),
+            ("Content-Type", "application/json"),
+            ("checksum", jsonChecksum)
+        ])
 
-
-    options = {
-
-        "method": "POST",
-
-        "uri": client.buildUrl(),
-
-        "headers": {
-
-            "apikey": keyPair.apiKey,
-
-            "Content-Type": "application/json",
-
-            "checksum": jsonChecksum
-
-        },
-
-        "body": jsonData,
-
-        "json": True
-
-    }
-
-
-
+    options = OrderedDict([
+        ("method", "POST"),
+        ("uri", client.buildUrl()),
+        ("headers", headersDict),
+        ("body", jsonData),
+        ("json", True)
+    ])
+    
     try:
-
         r = requests.post(options.get("uri"), headers=options.get("headers"), data=jsonData)
-
         return r.text
 
     except:
-
         print(r.status_code)
 
-
-
 #Serialization
-
 def serialize(projectData):
-
     dataToSend = json.dumps(OrderedDict([("Project", projectData.project),
                                          ("DateTime", projectData.datetime),
                                          ("Devices", projectData.serializeDevices()),
@@ -76,18 +44,10 @@ def serialize(projectData):
                                          ("Geolocation", projectData.geolocation)]),
                             separators=(',', ':'))
 
-    #NOT SURE
-
-    hashdata = hashlib.sha1(json.dumps(dataToSend, separators=(',', ':')).encode('UTF-8')).hexdigest()
-
-    serialized = {
-
-        "data": dataToSend,
-
-        "checksum": hashdata
-
-    }
-
-
+    hashdata = hashlib.sha1((dataToSend).encode('UTF-8')).hexdigest()
+    serialized = OrderedDict([
+        ("data", dataToSend),
+        ("checksum", hashdata)
+    ])
 
     return serialized
